85b0b24cc99a72fbeff80163f0d25f08dff279aa
unknown <facundoacostayl@outlook.com>
2023-02-15 16:48:58 +0100

    Add responseHandler to roleAuthorization middleware

--- src/middleware/roleAuthorization.ts
+++ src/middleware/roleAuthorization.ts
@@ -1,14 +1,13 @@
 import { Response, NextFunction } from "express";
 import { RequestExt } from "../interfaces/requestExt.interface";
 import { UserType } from "../interfaces/user.interface";
+import { responseHandler } from "../utils/response.handle";
 
 const authRole = (req: RequestExt, res: Response, next: NextFunction) => {
   const user = req.user as UserType;
-
   if (user.role !== "admin") {
-    return res.status(400).send("Only admin users allowed");
+    return responseHandler("Error", 401, "Just users with Admin role allowed");
   }
-
   next();
 };
 
