45c9c90715e9c40fc77f72efaabdccce9cde483b
facundoacostayl <facundoacostayl@outlook.com>
2023-03-06 07:33:57 -0300

    Add config file in config folder with db data

---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const DB_HOST = process.env.PROD_DB_HOST || process.env.DEV_DB_HOST;
+const DB_USER = process.env.PROD_DB_USER || process.env.DEV_DB_USERNAME;
+const DB_PASSWORD = process.env.PROD_DB_PASSWORD || process.env.DEV_DB_PASSWORD;
+const DB_NAME = process.env.PROD_DB_DATABASE || process.env.DEV_DB_DATABASE;
+const DB_PORT = process.env.PROD_DB_PORT || process.env.DEV_DB_PORT;
+
+export { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT };
--- src/config/db/db.ts
+++ src/config/db/db.ts
@@ -2,16 +2,15 @@ import { DataSource } from "typeorm";
 import { User } from "../entities/User";
 import { Client } from "../entities/Client";
 import { Operation } from "../entities/Operation";
-import * as dotenv from "dotenv";
-dotenv.config();
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from "../config";
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
-  port: 5432,
-  username: process.env.dbusername,
-  database: process.env.dbdatabase,
-  password: process.env.dbpassword,
+  host: DB_HOST,
+  port: DB_PORT ? parseInt(DB_PORT) : 5432,
+  username: DB_USER,
+  database: DB_NAME,
+  password: DB_PASSWORD,
   entities: [User, Client, Operation],
   logging: true,
   synchronize: true,
