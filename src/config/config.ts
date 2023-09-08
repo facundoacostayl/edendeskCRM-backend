a24b55dc81d7bbf46130acc44640e06748af9e42
facundoacostayl <facundoacostayl@outlook.com>
2023-09-08 07:21:31 -0300

    Update db config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,11 @@
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const DB_HOST = process.env.PROD_DB_HOST || process.env.DEV_DB_HOST;
-const DB_USER = process.env.PROD_DB_USER || process.env.DEV_DB_USERNAME;
-const DB_PASSWORD = process.env.PROD_DB_PASSWORD || process.env.DEV_DB_PASSWORD;
-const DB_NAME = process.env.PROD_DB_DATABASE || process.env.DEV_DB_DATABASE;
-const DB_PORT = process.env.PROD_DB_PORT || process.env.DEV_DB_PORT;
+const DB_HOST = "dpg-cjtf9d15mpss73d723dg-a" || process.env.DEV_DB_HOST;
+const DB_USER = "edenbest_user" || process.env.DEV_DB_USERNAME;
+const DB_PASSWORD =
+  "KMHTMTYA2z4bRU81F4RlmFKUDuSl7qfr" || process.env.DEV_DB_PASSWORD;
+const DB_NAME = "edenbest" || process.env.DEV_DB_DATABASE;
+const DB_PORT = "5432" || process.env.DEV_DB_PORT;
 
 export { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT };
